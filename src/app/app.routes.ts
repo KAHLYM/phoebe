97a49a958a4a119ecb300fa6abc68ea3d8e95217
KAHLYM <wrightci01s@gmail.com>
2024-02-25 10:57:23 +0000

    Add components to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PasscodeComponent } from './passcode/passcode.component';
+import { ReasonComponent } from './reason/reason.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'passcode', component: PasscodeComponent },
+    { path: 'reason', component: ReasonComponent },
+];
