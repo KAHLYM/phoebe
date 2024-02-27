28a525d958e4d7203ddbc7a3c98a155e4ecc7860
Callum Wright <wrightci01s@gmail.com>
2024-02-27 21:43:07 +0000

    Use regex to determine isNumberKey for KeyboardEvent (#9)

--- src/app/passcode-input/passcode-input.component.ts
+++ src/app/passcode-input/passcode-input.component.ts
@@ -20,7 +20,7 @@ export class PasscodeInputComponent {
   }
 
   private isNumberCode(event: KeyboardEvent): boolean {
-    return ["Digit0", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9"].includes(event.code);
+    return /^(Digit|Numpad)\d$/.test(event.code);
   }
 
   public isTrue(): boolean {
